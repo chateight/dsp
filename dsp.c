5b80c87d601742eb4e43c0ab27f287187a941c21
rusami <isehara.jp@coderdojo.com>
2025-07-02 07:45:19 +0900

    to refactor the code

--- dsp.c
+++ dsp.c
@@ -101,7 +101,7 @@ void __not_in_flash_func(adc_capture_edge)(uint16_t *buf, size_t count)
         }
     }
 
-    for (size_t i = 0; i < count; i = i + 1)
+    for (size_t i = 0; i < count; i++)
         buf[i] = adc_fifo_get_blocking();
     adc_run(false);
     adc_fifo_drain();
